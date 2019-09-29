bfc6b3d5fcfd713e643ca93bcd956a3c20387549
ChaCha <ckalswl331@naver.com>
2019-09-29 21:04:04 +0900

    Update facebook.py

--- web/facebook.py
+++ web/facebook.py
@@ -19,10 +19,10 @@ import urllib3
 
 url = sys.argv[1]
 
-app_id = "126860411384620"
-app_secret ="ed868a202489e08e2ad0d010b31dfd20"
+app_id = "..."
+app_secret ="..."
 access_token = app_id + "|" + app_secret
-page_id = "899513713536105"
+page_id = "..."
 
 def unescape(s):
 	s = s.replace("&lt;", "<")
